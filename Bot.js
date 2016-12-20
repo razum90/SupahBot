4527e0fc8599af371b4f0d43bd6d99961b38482b
Rasmus <you@example.com>
2016-12-20 22:23:33 +0100

    Removing dots after description texts for concistency

--- Bot.js
+++ Bot.js
@@ -13,26 +13,26 @@ var commands = {
   },
   '!weather': {
     execute: getWeather,
-    description: 'get current weather for the given city, defaults to Stockholm.'
+    description: 'get current weather for the given city, defaults to Stockholm'
   },
   '!roll': {
     execute: roll,
-    description: 'roll from 1-100.'
+    description: 'roll from 1-100'
   },
   '!help': {
     execute: showHelp
   },
   '!words': {
     execute: countWordsByUser,
-    description: 'get the most popular words for user of the given username, defaults to your username.'
+    description: 'get the most popular words for user of the given username, defaults to your username'
   },
   '!queue': {
     execute: doQueue,
-    description: 'queue your song.'
+    description: 'queue your song'
   },
   '!voteskip': {
     execute: voteSkip,
-    description: 'vote to skip the current song.'
+    description: 'vote to skip the current song'
   },
   '!song': {
     execute: showSong,
